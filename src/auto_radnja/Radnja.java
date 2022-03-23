afa7ff7e39c42b37f044b9d85aa5de8635893d9c
Mixilino <nisavicmiha@gmail.com>
2022-03-23 19:14:12 +0100

    Dodat javadoc za Radnja interfejs i Vulkanizerska radnja klasu

--- src/auto_radnja/Radnja.java
+++ src/auto_radnja/Radnja.java
@@ -1,1 +1,34 @@
-package auto_radnja;import java.util.List;import auto_radnja.gume.AutoGuma;public interface Radnja {void dodajGumu(AutoGuma a);List<AutoGuma> pronadjiGumu(String markaModel);List<AutoGuma> vratiSveGume();}
+package auto_radnja;
+
+import java.util.List;
+import auto_radnja.gume.AutoGuma;
+
+/**
+ * Interfejs Radnja
+ * @author Mihailo Nisavic
+ *
+ * @version v1.0
+ */
+public interface Radnja {
+	/**
+	 * Metoda koja dodaje novu AutoGumu u radnju
+	 * @param a - AutoGuma koja se dodaje
+	 * 
+	 * @throws NullPointerException ako je uneta vrednost null
+	 * @throws RuntimeException ako uneta guma vec postoji u listi
+	 */
+	void dodajGumu(AutoGuma a);
+
+	/**
+	 * Metoda koja pronalazi gumu
+	 * @param markaModel String po kome se pretrazuje AutoGuma
+	 * @return trazeni objekat AutoGuma 
+	 */
+	List<AutoGuma> pronadjiGumu(String markaModel);
+
+	/**
+	 * Metoda koja vraca listu listu svih guma
+	 * @return Lista svih guma u radnji 
+	 */
+	List<AutoGuma> vratiSveGume();
+}
--- src/auto_radnja/VulkanizerskaRadnja.java
+++ src/auto_radnja/VulkanizerskaRadnja.java
@@ -1,5 +1,45 @@
 package auto_radnja;
 
-public class VulkanizerskaRadnja {
+import java.util.List;
+import java.util.LinkedList;
+import auto_radnja.gume.AutoGuma;
 
+
+/**
+ * Klasa Vulkanizerska radnja. Implementira interfejs Radnja
+ * 
+ * @author Mihailo Nisavic
+ *
+ * @version v1.0
+ */
+public class VulkanizerskaRadnja implements Radnja {
+	/**
+	 * Lista guma koje postoje trenutno u radnji
+	 */
+	private List<AutoGuma> gume = new LinkedList<AutoGuma>();
+
+	@Override
+	public void dodajGumu(AutoGuma a) {
+		if (a == null)
+			throw new NullPointerException("Guma ne sme biti null");
+		if (gume.contains(a))
+			throw new RuntimeException("Guma vec postoji");
+		gume.addFirst(a);
+	}
+
+	@Override
+	public List<AutoGuma> pronadjiGumu(String markaModel) {
+		if (markaModel == null)
+			return null;
+		List<AutoGuma> novaLista = new LinkedList<AutoGuma>();
+		for (int i = 0; i < gume.size(); i++)
+			if (gume.get(i).equals(markaModel))
+				novaLista.add(gume.get(i));
+		return novaLista;
+	}
+
+	@Override
+	public List<AutoGuma> vratiSveGume() {
+		return gume;
+	}
 }
